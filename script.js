9e02a60dd7febdf045fffac14d4d6ce7b1eaf7dd
21Kave <kauanvelasco10@gmail.com>
2023-06-05 16:16:15 -0300

    script

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ function togglemode() {
   const img = document.querySelector("#profile img")
 
   if(html.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar2.png')
-  } else{
+    img.setAttribute(
+      "src",
+      "https://raw.githubusercontent.com/21Kave/projetozero/main/Assets/Avatar2.png"
+    )
+  } else {
     img.setAttribute('src', './assets/avatar.png')
   }
 }
